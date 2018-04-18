808b2afb3c0988b293ff00bb9f9e344dec5fdae5
Olivier JM <olivierjm@Olivier-MacBook-Air.local>
2018-04-19 00:35:06 +0200

    modified results page

--- imports/ui/components/Results.jsx
+++ imports/ui/components/Results.jsx
@@ -1,13 +1,11 @@
 /* eslint class-methods-use-this: "off" */
 import React, { Component, Fragment } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
-import { PropTypes } from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { Services } from '../../api/services/services';
 
 export class Results extends Component {
   render() {
-    console.log(this.props.services);
     return (
       <Fragment>
         Results Page
